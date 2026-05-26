03840a5311c75d56d4398d634707800d05042b15
Phatthaphop Lalitpraphaphirom <phatthaphop@mfec.co.th>
2026-05-26 12:09:46 +0700

    Add token-spendie formula; update cask for .app from .zip installation

--- Casks/token-spendie.rb
+++ Casks/token-spendie.rb
@@ -2,17 +2,14 @@ cask "token-spendie" do
   version "0.0.6"
   sha256 "e924601eaab2c942488bed3c3339ab8d601e7d807c05d29cf358324e5492ef9d"
 
-  url "https://github.com/sensecherise/token-spendie/releases/download/v#{version}/TokenSpendie-#{version}.pkg"
+  url "https://github.com/sensecherise/token-spendie/releases/download/v#{version}/TokenSpendie-#{version}.zip"
   name "Token Spendie"
   desc "Claude Code usage menu bar widget"
   homepage "https://github.com/sensecherise/token-spendie"
 
   depends_on macos: :ventura
 
-  pkg "TokenSpendie-#{version}.pkg"
-
-  uninstall quit:    "com.cherise.TokenSpendie",
-            pkgutil: "com.cherise.TokenSpendie"
+  app "TokenSpendie.app"
 
   zap trash: [
     "~/Library/Application Support/TokenSpendie",
